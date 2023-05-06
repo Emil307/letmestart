1597983b0e8ece00160967fc3e7cbfc50a736542
Emil307 <emilnovikov392@gmail.com>
2023-05-06 13:57:07 +0600

    add routing

---
+++ src/pages/Words.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Words: React.FC = () => {
+  return (
+    <div>Words</div>
+  )
+}
+
+export default Words
---
+++ src/pages/WordsSet.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WordsSet: React.FC = () => {
+  return (
+    <div>WordsSet</div>
+  )
+}
+
+export default WordsSet
