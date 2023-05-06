a5ffb62ce941a41e50101d1854abf175317c4a4d
Emil307 <emilnovikov392@gmail.com>
2023-05-06 16:24:27 +0600

    styled h1

--- src/shared/H1.tsx
+++ src/shared/H1.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h1`
-  
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: 42px;
+  line-height: 48px;
 `
 
 interface H1Props {
