738a201ee75b93d3d0f62037c8f283b0828437ee
Emil307 <emilnovikov392@gmail.com>
2023-05-06 21:16:32 +0600

    create interactive progress bar

--- src/shared/ProgressBar.tsx
+++ src/shared/ProgressBar.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface ProgressBarProps {
-  progress?: number,
+  progress: number,
   height?: string,
   width?: string,
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress, height, width }) => {
+  const [activeProgress, setActiveProgress] = useState(0);
+  const [visor, setVisor] = useState(0);
+  
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setActiveProgress(activeProgress + 1);
+        if (progress !== activeProgress) {
+          setVisor(visor + 1)
+        }
+      }, 5);
+
+      return () => clearInterval(interval);
+  }, [visor]);
+
   return (
     <div style={{width: width, height: height, background: 'rgba(0, 0, 0, 0.05)', borderRadius: '10px'}}>
-      <div style={{width: `${progress}%`, height: height, background: '#000', borderRadius: '10px'}}></div>
+      <div style={{width: `${activeProgress}%`, height: height, background: '#000', borderRadius: '10px'}}></div>
     </div>
   )
 }
