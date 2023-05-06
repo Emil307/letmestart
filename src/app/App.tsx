b3d816074628f63110c1249c99292c33c192ae34
Emil307 <emilnovikov392@gmail.com>
2023-05-06 14:05:42 +0600

    create features sliced design

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Landing from '../pages/Landing';
+import Auth from '../pages/Auth';
+import SignUp from '../pages/SignUp';
+import Words from '../pages/Words';
+import WordsSet from '../pages/WordsSet';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/auth' element={<Auth/>} />
+        <Route path='/signUp' element={<SignUp/>} />
+        <Route path='/words' element={<Words/>} />
+        <Route path='/wordset' element={<WordsSet/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
